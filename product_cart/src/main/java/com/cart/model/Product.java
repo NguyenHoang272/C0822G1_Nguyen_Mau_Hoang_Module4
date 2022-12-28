b650284622fd8f98c4b09ac76f6710638568a9f8
NguyenHoang272 <nguyenhoang29082001@gmail.com>
2022-12-28 15:10:22 +0700

    ss

---
+++ product_cart/src/main/java/com/cart/model/Product.java
@@ -0,0 +1,73 @@
+package com.cart.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String image;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    private double price;
+    private int discount;
+
+    public Product() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+}
---
+++ product_cart/src/main/java/com/cart/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.cart.service;
+
+import com.cart.model.Product;
+import com.cart.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService{
+
+    @Autowired
+    private IProductRepository iproductRepository;
+    @Override
+    public List<Product> findAll() {
+        return iproductRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Integer id) {
+        return iproductRepository.findById(id);
+    }
+
+    @Override
+    public void save(Product product) {
+        iproductRepository.save(product);
+    }
+}
